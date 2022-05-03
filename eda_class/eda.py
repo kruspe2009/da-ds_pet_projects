1825045a4c5c7e08749db6b625c8ae10b070523f
Rodion Yangirov <74657341+kruspe2009@users.noreply.github.com>
2022-05-03 19:52:07 +0300

    Added a bit customised histogram graph constructor

--- eda_class/eda.py
+++ eda_class/eda.py
@@ -22,3 +22,29 @@ class eda:
         report.columns = ["Столбец", "Количество пропусков"]
         report["% от датасета"] = report["Количество пропусков"] / len(self)
         return report
+
+    def values_ditribution(
+        self, column, x_label="Распределение признака", title="Распределение", bins=20
+    ):
+        plt.figure(figsize=[16, 8])
+        plt.hist(x=self[column], bins=bins, ec="black")
+        plt.grid(axis="y", alpha=0.5)
+        plt.xlabel(x_label, fontsize=12)
+        plt.axvline(
+            x=data[(~self[column].isna())][column].median(),
+            color="r",
+            label=f"Медиана: {round(data[(~self[column].isna())][column].median(), 2)}",
+        )
+        plt.axvline(
+            x=np.percentile(self[column], 99),
+            color="g",
+            label=f"99й процентиль: {round(np.percentile(self[column], 99), 2)}",
+        )
+        plt.axvline(
+            x=data[(~self[column].isna())][column].mean(),
+            color="b",
+            label=f"Среднее: {round(data[(~self[column].isna())][column].mean(), 2)}",
+        )
+        plt.legend()
+        plt.title(title, fontsize=15)
+        plt.show()
